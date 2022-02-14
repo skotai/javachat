713845e5450b8cd1218c232f3e36774a2f0274d1
skotai <44361993+skotai@users.noreply.github.com>
2022-02-15 00:15:32 +0900

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,39 +1,31 @@
 import java.util.*;
 import java.net.*;
 import java.io.*;
-class User
-{
+class User{
 	private String name;//用户姓名
 	private String sex;//用户性别
 	private Socket sock;//用户自己的socket
-	public User(String name,String sex,Socket sock)
-	{
+	public User(String name,String sex,Socket sock){
 		setName(name);
 		setSex(sex);
 		setSock(sock);
 	}
-	public String getName()
-	{
+	public String getName(){
 		return name;
 	}
-	public void setName(String name)
-	{
+	public void setName(String name){
 		this.name=name;
 	}
-	public String getSex()
-	{
+	public String getSex(){
 		return sex;
 	}
-	public void setSex(String sex)
-	{
+	public void setSex(String sex){
 		this.sex=sex;
 	}
-	public Socket getSock()
-	{
+	public Socket getSock(){
 		return sock;
 	}
-	public void setSock(Socket sock)
-	{
+	public void setSock(Socket sock){
 		this.sock=sock;
 	}
 	
